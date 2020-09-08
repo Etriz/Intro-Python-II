0261198d93ce536f51ab20f70de193c8bb2389e1
ryanp <ryanpanimation@gmail.com>
2020-09-07 23:54:02 -0500

    player and room classes made

--- src/player.py
+++ src/player.py
@@ -1,2 +1,13 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, current_room):
+        self.current_room = current_room
+
+    def __str__(self):
+        return f"You are in the {self.current_room} room"
+
+    def __repr__(self):
+        return f"This is the {self.current_room}"
